b97c3240442350895dfcc74429ed2c3e2a1e5682
Vahe Abazyan <va.abazyan@gmail.com>
2022-04-06 17:03:23 +0400

    Initial Commit

---
+++ CoinStatsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinStatsTask
+//
+//  Created by Vahe Abazyan on 06.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
